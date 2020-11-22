b194aa9061fe83060725cbd94ddb4f9e498dcb23
carlos <carlos.20499@gmail.com>
2020-11-22 20:22:51 -0300

    add function shoppingCartTotal

--- js/index.js
+++ js/index.js
@@ -35,10 +35,10 @@ function addCartClick(e){
 }
 
 function addItemToShopCart(textProductName, textImage, textPrice){
-  console.log(textProductName)
+  // console.log(textProductName)
   const shoppingCartRow = document.createElement('div');
   const shoppingCartContent = `
-    <div class="row">
+    <div class="row shoppingCartItem">
       <div class="columnas">
         <div>
           <img class="popup-shopCart" src="${textImage}" alt="">
@@ -46,22 +46,51 @@ function addItemToShopCart(textProductName, textImage, textPrice){
         </div>
       </div>
       <div class="columnas">
-        <div>
-        <p> ${textPrice} </p>
+        <div >
+        <p class="shoppingCartPrice" > ${textPrice} </p>
         </div>
       </div>
       <div class="columnas">
         <div>
-        <input type="number">
-        <button type="button" ></button>
+        <input type="number" class="shoppingCartItemQuantity" value="1">
+        <button type="button" class="button-delete" > eliminar</button>
         </div>
       </div>
     </div>
   `;
-  shoppingCartRow.innerHTML = shoppingCartContent
-  shoppingCart.append(shoppingCartRow)
+  shoppingCartRow.innerHTML = shoppingCartContent;
+  shoppingCart.append(shoppingCartRow);
+  shoppingCartRow.querySelector('.button-delete').addEventListener('click', removeShoppingCartItem);
+  shoppingCartRow.querySelector('.shoppingCartItemQuantity').addEventListener('change', ()=> console.log('change'))
+
+  shoppingCartTotal();
 }
 
+function removeShoppingCartItem(e){
+  const buttonClick = e.target;
+  buttonClick.closest('.shoppingCartItem').remove();
+  shoppingCartTotal()
+  console.log(buttonClick)
+
+}
+
+
+function shoppingCartTotal (){
+
+  let total = 0
+  const shoppingCartTotalLabel = document.querySelector('.shopping-Cart-total')
+  const shoppingCartItem = document.querySelectorAll('.shoppingCartItem')
+  
+  shoppingCartItem.forEach(item => {
+    const shoppingCartPriceElement = item.querySelector('.shoppingCartPrice')
+    const shoppingCartPrice = Number(shoppingCartPriceElement.textContent.replace('$', ''))
+    const shoppingCartItemQuantityElement = item.querySelector('.shoppingCartItemQuantity')
+    const shoppingCartItemQuantity = Number(shoppingCartItemQuantityElement.value)
+    total = total + shoppingCartItemQuantity * shoppingCartPrice
+  })
+  shoppingCartTotalLabel.innerHTML = `TOTAL $ ${total}`
+  console.log(total)
+}
 
 function createCard(result){
   const main = document.getElementById("main")
@@ -113,41 +142,6 @@ function createCard(result){
     return response.json();
   })
 
-}
-
-function genera_tabla() {
-  // Obtener la referencia del elemento body
-  var body = document.getElementsByTagName("body")[0];
- 
-  // Crea un elemento <table> y un elemento <tbody>
-  var tabla   = document.createElement("table");
-  var tblBody = document.createElement("tbody");
- 
-  // Crea las celdas
-  for (var i = 0; i < 2; i++) {
-    // Crea las hileras de la tabla
-    var hilera = document.createElement("tr");
- 
-    for (var j = 0; j < 2; j++) {
-      // Crea un elemento <td> y un nodo de texto, haz que el nodo de
-      // texto sea el contenido de <td>, ubica el elemento <td> al final
-      // de la hilera de la tabla
-      var celda = document.createElement("td");
-      var textoCelda = document.createTextNode("celda en la hilera "+i+", columna "+j);
-      celda.appendChild(textoCelda);
-      hilera.appendChild(celda);
-    }
- 
-    // agrega la hilera al final de la tabla (al final del elemento tblbody)
-    tblBody.appendChild(hilera);
-  }
- 
-  // posiciona el <tbody> debajo del elemento <table>
-  tabla.appendChild(tblBody);
-  // appends <table> into <body>
-  body.appendChild(tabla);
-  // modifica el atributo "border" de la tabla y lo fija a "2";
-  tabla.setAttribute("border", "2");
 }
 
 
